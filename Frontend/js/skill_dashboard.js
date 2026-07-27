b718398c6e65a959c35be1f23973c0c13c9f44de
S-Astralcoder <suraj.astralcode.ai@gmail.com>
2026-07-27 22:24:14 +0530

    Add project and task dialog functionality with corresponding HTML, CSS, and JavaScript. Implement modal dialogs for creating new projects and tasks, enhancing user interaction and experience across the dashboard.

---
+++ Frontend/js/skill_dashboard.js
@@ -0,0 +1,6 @@
+
+const create_task_btn = document.querySelector("#create-task")
+const skill_model = document.querySelector("#task-dialog")
+
+create_task_btn.addEventListener("click", () => {skill_model.showModal()})
+
---
+++ Frontend/js/task_dashboard.js
@@ -0,0 +1,4 @@
+const create_project_btn = document.querySelector("#create-project")
+const skill_model = document.querySelector("#project-dialog")
+
+create_project_btn.addEventListener("click", () => {skill_model.showModal()})
